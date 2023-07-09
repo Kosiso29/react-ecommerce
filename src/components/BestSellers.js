cf8b4303860466753a2b03fe218cbab395f020a0
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2023-07-09 20:11:50 +0100

    9/6/23: update bestseller navbar tab

--- src/components/BestSellers.js
+++ src/components/BestSellers.js
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { store } from '../productsStore/Store'
 import "../styles/BestSellers.css";
 import HoverImage from 'react-hover-image/build';
 import { Link } from 'react-router-dom';
+import logo from "../assets/cara.png";
 
 const BestSellers = () => {
+    const [state, setState] = useState({ results: [] });
+
+    const getData = async (pageNumber) => {
+        await new Promise((resolve, reject) => {
+            fetch('http://solarsales.pythonanywhere.com/products/?ordering=-rating&page_size=4')
+                .then(response => response.json())
+                .then(data => {
+                    setState(data)
+                    resolve()
+            }).catch(() => reject())
+        })
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
   return (
     <div className='bestSellerMainParent flex flex-row'>
-      {store.map((item) => {
-        if (item.type == "bestSeller")
+      {state?.results?.map((item) => {
           return (
             <Link to={`/${item.id}`} key={item.id}>  
             <div key={item.id} className="bestSellerIndivitualItem">
-              <HoverImage src={item.primaryImage} hoverSrc={item.hoverImg}   className="bestSellerImage rounded-xl mb-6"/>
+              <HoverImage src={item.image} hoverSrc={logo}   className="bestSellerImage rounded-xl mb-6"/>
               <p className='bestSellerName text-center mb-2'> {item.name} </p>
               <p className=' font-normal text-center'> ${item.price} </p>
             </div>
--- src/components/Products.js
+++ src/components/Products.js
@@ -22,7 +22,7 @@ const Products = () => {
             }).catch(() => reject())
         })
 
-        setActivePageNumber(pageNumber);
+        setActivePageNumber(pageNumber || activePageNumber);
     }
 
     useEffect(() => {

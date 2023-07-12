5df47359279c631871a02972cc088ed1f7c221ba
unknown <nonsodavilo@gmail.com>
2023-07-12 02:45:35 -0700

    edited body.js to have names of Panel, Battery and Inverter. we need to make it dynamic

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,3 @@
-
-
 import React from 'react'
 import "../styles/Body.css";
 import { useState } from 'react';
@@ -12,15 +10,13 @@ const Body = () => {
     const [show, setShow] = useState(true);
     const [show2, setShow2] = useState(false);
     const [show3, setShow3] = useState(false);
-    const [show4, setShow4] = useState(false);
 
 
-    const showHandler = () => {
+    const showHandler = async() => {
         setShow(true)
         document.querySelector(".bodyContent").classList.add("transition");
         setShow2(false)
         setShow3(false)
-        setShow4(false)
 
         document.querySelector(".bodyContent").style.opacity = "1";
         document.querySelector(".hairContent").style.opacity = "0";
@@ -29,13 +25,11 @@ const Body = () => {
 
     }
 
-    const showHandler2 = () => {
+    const showHandler2 = async() => {
         setShow2(true)
         document.querySelector(".hairContent").classList.add("transition2");
         setShow(false)
         setShow3(false)
-        setShow4(false)
-
 
         document.querySelector(".hairContent").style.opacity = "1";
         document.querySelector(".bodyContent").style.opacity = "0";
@@ -44,14 +38,11 @@ const Body = () => {
 
     }
 
-    const showHandler3 = () => {
+    const showHandler3 = async() => {
         setShow3(true)
         document.querySelector(".travelContent").classList.add("transition3");
-        //document.querySelector(".hairContent").style.opacity = "0";
-        //document.querySelector(".newContent").style.opacity="0"
         setShow(false)
         setShow2(false)
-        setShow4(false)
 
         document.querySelector(".travelContent").style.opacity = "1";
         document.querySelector(".hairContent").style.opacity = "0";
@@ -59,26 +50,10 @@ const Body = () => {
         document.querySelector(".newContent").style.opacity = "0";
     }
 
-    const showHandler4 = () => {
-        setShow4(true)
-        document.querySelector(".newContent").classList.add("transition4");
-        //document.querySelector(".travelContent").style.opacity = "0";
-        setShow(false)
-        setShow2(false)
-        setShow3(false)
-
-        document.querySelector(".newContent").style.opacity = "1";
-        document.querySelector(".hairContent").style.opacity = "0";
-        document.querySelector(".bodyContent").style.opacity = "0";
-        document.querySelector(".travelContent").style.opacity = "0";
-    }
-
-
     const dontShowHandler = () => {
         setShow(false)
         setShow2(false)
         setShow3(false)
-        setShow4(false)
 
         document.querySelector(".newContent").style.opacity = "0";
         document.querySelector(".hairContent").style.opacity = "0";
@@ -98,19 +73,11 @@ const Body = () => {
                     <p className=' font-semibold hover-underline-animation'>BATTERY</p>
                 </div>
 
-                <div className='travelHold w-52 lineHeight' onMouseMove={showHandler4}>
+                <div className='travelHold w-52 lineHeight' onMouseMove={showHandler3}>
                     <p className=' font-semibold hover-underline-animation'>INVERTER</p>
                 </div>
 
-                <div className='newHold lineHeight' onMouseMove={showHandler3}>
-                    <p className=' font-semibold hover-underline-animation'>OTHERS</p>
-                </div>
-
-
-
             </div>
-
-
 
             {show && <div className=' z-50 bodyContent color text-base' onMouseLeave={dontShowHandler}>
                 <p>   100W </p>
@@ -146,16 +113,6 @@ const Body = () => {
                     <p>  AGM </p>
                 </div>
 
-
-                <div className='hairTypeCategoryTWO flex-col flex gap-10'>
-                    <p className='font-bold'>  AH </p>
-                    <p>  100AH </p>
-                    <p>  200AH </p>
-                    <p>  220AH </p>
-                    <p>  2.5KWH </p>
-                    <p>  5.1KWH </p>
-                </div>
-
                 <div className='hairImageHold flex flex-row gap-10'>
                     <img src='https://cdn.shopify.com/s/files/1/0081/7374/8305/articles/Natural_Balance_Shampoo_02_540x.jpg?v=1550710011' className=' rounded-xl w-52' />
                     <img src='https://cdn.shopify.com/s/files/1/0081/7374/8305/articles/ee7ea9c87918e493665a3a84bdf6c00a_large_1b25100f-7d60-451b-9d01-e74d104141d5_540x.jpg?v=1550710009' className=' rounded-xl w-52' />
@@ -168,29 +125,8 @@ const Body = () => {
 
             </div>}
 
-            {show3 && <div className='z-50 travelContent' onMouseLeave={dontShowHandler}>
-                <div className='flex flex-row'>
-                    <div className='TravelStoreHold flex flex-row gap-16'>
-                        {store.map((item) => {
-                            if (item.type == "navbar-TravelType") {
-                                return (
-                                    <Link to={`/${item.id}`} key={item.id}>
 
-                                        <div key={item.id} className="TravelIndivitual">
-                                            <HoverImage src={item.primaryImage} hoverSrc={item.hoverImg} className=" rounded-xl w-40" />
-                                            <p className=' font-semibold text-base px'> {item.name} </p>
-                                            <p className='text-base font-normal text-center'> ₦{item.price} </p>
-                                        </div>
-                                    </Link>
-                                )
-                            }
-                        })}
-                    </div>
-
-                </div>
-            </div>}
-
-            {show4 && <div className='newContent relative left-72 text-base text-gray-500 top-16' onMouseLeave={dontShowHandler}>
+            {show3 && <div className='newContent relative left-72 text-base text-gray-500 top-16' onMouseLeave={dontShowHandler}>
                 <div className='newCategoryHold flex flex-col gap-9'>
                     <p className='font-bold text-xl'>  CATEGORY </p>
 

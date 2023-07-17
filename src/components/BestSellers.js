b55b17c138f17a48b23033c57946f74756fad47b
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2023-07-17 10:43:40 +0100

    17/7/23: normalize BestSeller hoverImage

--- src/components/BestSellers.js
+++ src/components/BestSellers.js
@@ -3,7 +3,6 @@ import { store } from '../productsStore/Store'
 import "../styles/BestSellers.css";
 import HoverImage from 'react-hover-image/build';
 import { Link } from 'react-router-dom';
-import logo from "../assets/cara.png";
 import axios from "../axios";
 
 const BestSellers = () => {
@@ -36,7 +35,7 @@ const BestSellers = () => {
           return (
             <Link to={`/${item.id}`} key={item.id}>  
             <div key={item.id} className="bestSellerIndivitualItem">
-              <HoverImage src={item.image} hoverSrc={logo}   className="bestSellerImage rounded-xl mb-6"/>
+              <HoverImage src={item.image} hoverSrc={item.image}   className="bestSellerImage rounded-xl mb-6"/>
               <p className='bestSellerName text-center mb-2'> {item.name} </p>
               <p className=' font-normal text-center'> {formatter.format(item.price)} </p>
             </div>

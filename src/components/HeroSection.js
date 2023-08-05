f538b60fb21016eac65b2ed2473394832795219b
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2023-08-05 14:03:59 +0100

    5/8/23: fix heroSection component in smaller screens

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -11,21 +11,21 @@ const HeroSection = () => {
 
             <p className='text-4xl mt-20 text-center'> We sell inverter and inverter batteries, new and fairly used as well as solar products and equipments. </p>
             <div className='flex flex-wrap justify-around mt-20'>
-                <div className='flex flex-wrap justify-center mx-3 my-6'>
+                <div className='hero-section-child flex flex-wrap justify-center mx-3 my-6'>
                     <img src={one} className=" w-20" alt='' />
                     <div className='ml-6'>
                         <p className='text-xl font-medium mb-3'> Powered By Nature </p>
                         <p> Harness the power of the sun, <br /> Radiance of solar energy. </p>
                     </div>
                 </div>
-                <div className='flex flex-wrap justify-center mx-3 my-6'>
+                <div className='hero-section-child flex flex-wrap justify-center mx-3 my-6'>
                     <img src={two} className=" w-20" alt='' />
                     <div className='ml-6'>
                         <p className='text-xl font-medium mb-3'> Exceptional Quality </p>
                         <p> Exceptional quality shines as, <br /> brightly as the sun. </p>
                     </div>
                 </div>
-                <div className='flex flex-wrap justify-center mx-3 my-6'>
+                <div className='hero-section-child flex flex-wrap justify-center mx-3 my-6'>
                     <img src={three} className=" w-20" alt=''/>
                     <div className='ml-6'>
                         <p className='text-xl font-medium mb-3'> Home Friendly </p>

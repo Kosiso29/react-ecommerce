602eee51465c1ca1d2ee3d71ef83e3cf948e3469
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2023-08-05 09:05:19 +0100

    5/8/23: fix HeroSection component

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,27 +7,31 @@ import three from "../assets/10_122764.png"
 
 const HeroSection = () => {
     return (
-        <div className='heroSecMainParent'>
+        <div className='heroSecMainParent mx-24'>
 
             <p className='text-4xl mt-20 text-center welcomStore'> We sell inverter and inverter batteries, new and fairly used as well as solar products and equipments. </p>
-
-
-            <div className='heroPicHold flex flex-row '>
-                <img src={one} className=" w-20" alt=''/>
-                <img src={two} className=" w-20" alt='' />
-                <img src={three} className=" w-20" alt=''/>
-            </div>
-
-            <div className=' text-xl font-medium flex flex-row heroHeadingHold'>
-                <p> Powered By Nature </p>
-                <p> Exceptional Quality </p>
-                <p> Home Friendly </p>
-            </div>
-
-            <div className='heroDescHold flex-row flex relative text-base font-normal'>
-                <p> Harness the power of the sun, <br /> Radiance of solar energy. </p>
-                <p> Exceptional quality shines as, <br /> brightly as the sun. </p>
-                <p> Making homes friendlier to both <br /> our planet and our pockets.</p>
+            <div className='flex flex-wrap justify-around mt-20'>
+                <div className='flex flex-wrap justify-between mx-3 my-6'>
+                    <img src={one} className=" w-20" alt='' />
+                    <div className='ml-6'>
+                        <p className='text-xl font-medium mb-3'> Powered By Nature </p>
+                        <p> Harness the power of the sun, <br /> Radiance of solar energy. </p>
+                    </div>
+                </div>
+                <div className='flex flex-wrap justify-between mx-3 my-6'>
+                    <img src={two} className=" w-20" alt='' />
+                    <div className='ml-6'>
+                        <p className='text-xl font-medium mb-3'> Exceptional Quality </p>
+                        <p> Exceptional quality shines as, <br /> brightly as the sun. </p>
+                    </div>
+                </div>
+                <div className='flex flex-wrap justify-between mx-3 my-6'>
+                    <img src={three} className=" w-20" alt=''/>
+                    <div className='ml-6'>
+                        <p className='text-xl font-medium mb-3'> Home Friendly </p>
+                        <p> Making homes friendlier to both <br /> our planet and our pockets.</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
